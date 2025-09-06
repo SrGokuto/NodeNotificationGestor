0b5c8c7724599bc6bdd1e99313e58855edcb1e59
SrGokuto <78231110+SrGokuto@users.noreply.github.com>
2025-09-06 16:51:14 -0500

    Delete types directory (invalid file naming structure)

--- types/types.ts
+++
@@ -1,7 +0,0 @@
-// | [] <>
-
-type User = string;
-type TiposNotificacion = "sistema" | "ocio" | "volumen"
-type Notificacion = {tipo: TiposNotificacion, message: string}
-
-export {User, Notificacion}
